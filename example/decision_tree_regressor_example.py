c6fbd33ed6a6cac2c4c18ba1f5105ee51d9a6b74
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2023-08-21 17:24:53 +0700

    bug fixes

--- example/decision_tree_regressor_example.py
+++ example/decision_tree_regressor_example.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Create and train the decision tree
-    tree = DecisionTreeRegressor(max_depth=3)
+    tree = DecisionTreeRegressor()
     tree.fit(X_train, y_train)
 
     # Make predictions
